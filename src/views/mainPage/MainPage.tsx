af6763d1246421875780a692c7f112c6cad4db4a
Jan Metelański <metelanskij@gmail.com>
2025-03-30 18:08:01 +0200

    update

--- src/views/mainPage/MainPage.tsx
+++ src/views/mainPage/MainPage.tsx
@@ -3,7 +3,7 @@ import Navigation from '../../components/navigation/Navigation';
 
 const MainPage: React.FC = () => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex flex-col gap-y-16 md:flex-row md:gap-y-0 md:gap-x-4 justify-between'>
       <Navigation />
       <About />
     </div>
